bd6f4a3e3fdbc502f28ef3734a25f9d39fe62c4a
RobinBoisseau <rboisseau001@iutbayonne.univ-pau.fr>
2026-04-23 15:44:44 +0200

    Modification affichage exercice

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
 import { ExerciceListComponent } from './components/exercice-list/exercice-list.component';
+import { ExerciceDetailComponent } from './components/exercice-detail/exercice-detail.component';
 
 export const routes: Routes = [
   { path: 'exercices', component: ExerciceListComponent },
-  { path: '', redirectTo: 'exercices', pathMatch: 'full' }
+  
+  // Correction : Ajout de la virgule après 'full' }
+  { path: '', redirectTo: 'exercices', pathMatch: 'full' }, 
+  
+  { path: 'exercice/:id', component: ExerciceDetailComponent },
 ];
