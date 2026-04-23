dbd6722453eee87b889e41b2657d16804c977a5c
RobinBoisseau <rboisseau001@iutbayonne.univ-pau.fr>
2026-04-23 15:11:37 +0200

    Amélioration de l'ergonomie pour éviter d'avoir une liste d'énoncés

--- frontend/src/app/components/exercice-list/exercice-list.component.ts
+++ frontend/src/app/components/exercice-list/exercice-list.component.ts
@@ -1,27 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AngularSplitModule } from 'angular-split'; // Importation
+import { AngularSplitModule } from 'angular-split';
 import { ExerciceService } from '../../services/exercice.service';
 import { Exercice } from '../../models/exercice';
 
 @Component({
   selector: 'app-exercice-list',
   standalone: true,
-  imports: [
-    CommonModule, 
-    AngularSplitModule // Ajout ici
-  ],
+  imports: [CommonModule, AngularSplitModule],
   templateUrl: './exercice-list.component.html',
   styleUrls: ['./exercice-list.component.css']
 })
 export class ExerciceListComponent implements OnInit {
   exercices: Exercice[] = [];
+  
+  // On utilise l'index de la liste pour l'accordéon
+  selectedIndex: number | null = null; 
 
   constructor(private exerciceService: ExerciceService) {}
 
   ngOnInit(): void {
-    this.exerciceService.getExercices().subscribe(data => {
-      this.exercices = data;
+    this.exerciceService.getExercices().subscribe({
+      next: (data) => {
+        this.exercices = data;
+      },
+      error: (err) => console.error('Erreur API Laravel:', err)
     });
+  }
+
+  // Ouvre ou ferme l'exercice cliqué
+  toggleExercise(index: number): void {
+    this.selectedIndex = (this.selectedIndex === index) ? null : index;
   }
 }
