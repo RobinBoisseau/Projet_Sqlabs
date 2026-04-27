8ef6147b063ebe5119a92fb2d4c07ede4c315de2
RobinBoisseau <rboisseau001@iutbayonne.univ-pau.fr>
2026-04-27 15:54:30 +0200

    CSS Header

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 import { Router, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
-  imports: [CommonModule, RouterLink], // On ajoute les outils nécessaires
+  imports: [CommonModule, RouterLink], 
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
@@ -15,10 +13,8 @@ export class HeaderComponent {
   firstName: string = 'User';
   lastName: string = 'User';
 
-  // Le constructeur doit être au début de la classe
   constructor(public router: Router) {}
 
-  // Cette fonction renvoie "true" seulement si on est sur la page d'un exercice
   isOnDetail(): boolean {
     return this.router.url.includes('/exercice/');
   }
