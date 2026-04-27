ef6ab4c4f2836009b71391be4d2de6cbdf6e399e
RobinBoisseau <rboisseau001@iutbayonne.univ-pau.fr>
2026-04-27 15:34:41 +0200

    Changement Header

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
+  imports: [CommonModule, RouterLink], // On ajoute les outils nécessaires
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
@@ -10,9 +13,15 @@ export class HeaderComponent {
   firstName: string = 'User';
   lastName: string = 'User';
 
+  // Le constructeur doit être au début de la classe
+  constructor(public router: Router) {}
+
+  // Cette fonction renvoie "true" seulement si on est sur la page d'un exercice
+  isOnDetail(): boolean {
+    return this.router.url.includes('/exercice/');
+  }
+
   onProfileClick() {
-    console.log("Ouverture du profil... (Fonctionnalité à venir)");
-    // Tu peux ajouter une alerte pour montrer que ça marche
-    // alert('Bientôt vous pourrez voir votre profil !');
+    console.log("Ouverture du profil...");
   }
 }
