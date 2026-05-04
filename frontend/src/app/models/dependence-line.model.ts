0ec57978bcdf1c60af92d91448e68ea403eb529b
RobinBoisseau <rboisseau001@iutbayonne.univ-pau.fr>
2026-05-04 17:26:34 +0200

    Correction erreurs Dependence line

--- frontend/src/app/models/dependence-line.model.ts
+++ frontend/src/app/models/dependence-line.model.ts
@@ -1,14 +1,14 @@
 export class DependenceLine {
   constructor(
     public id: string = '',
-    public source: string = '',
+    public source: string[] = [],
     public cible: string[] = []
   ) {}
- 
+
   static fromJSON(data: any): DependenceLine {
     return new DependenceLine(
       data.id,
-      data.source,
+      data.source ?? [],  
       data.cible ?? []
     );
   }
