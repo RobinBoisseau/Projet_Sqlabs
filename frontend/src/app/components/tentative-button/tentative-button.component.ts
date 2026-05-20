90fb029faa054df913ab350a2c5f001b436de955
RobinBoisseau <rboisseau001@iutbayonne.univ-pau.fr>
2026-05-20 09:45:06 +0200

    TQT

---
+++ frontend/src/app/components/tentative-button/tentative-button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tentative-button',
+  imports: [],
+  templateUrl: './tentative-button.component.html',
+  styleUrl: './tentative-button.component.css'
+})
+export class TentativeButtonComponent {
+
+}
