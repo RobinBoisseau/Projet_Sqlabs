78d0668ce1f509bcfd22458dc2f1888e320fecb6
RobinBoisseau <rboisseau001@iutbayonne.univ-pau.fr>
2026-05-21 14:04:43 +0200

    Supression des labels au survol des zonnes

--- frontend/src/app/components/panel/panel.component.ts
+++ frontend/src/app/components/panel/panel.component.ts
@@ -9,6 +9,6 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./panel.component.css']
 })
 export class PanelComponent {
-  @Input() title: string = ''; // Le titre de la colonne
-  @Input() scrollable: boolean = true; // Si on veut le scroll ou pas
+  @Input() title: string = '';
+  @Input() scrollable: boolean = true;
 }
